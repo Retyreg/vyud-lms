2530d9d70262d7b7a6ca90b7dff85bd26adcfa5b
Retyreg <45520270+Retyreg@users.noreply.github.com>
2026-03-11 18:43:37 +0300

    Simplify next.config.ts by removing options

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,3 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    // Это позволит завершить сборку, даже если в коде есть ошибки линтинга
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Это позволит завершить сборку, даже если есть ошибки типов (полезно для быстрого деплоя)
-    ignoreBuildErrors: true,
-  },
-};
-
+const nextConfig: NextConfig = {};
 export default nextConfig;
